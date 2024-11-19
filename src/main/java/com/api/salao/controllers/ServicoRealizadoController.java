a16df64c9a8a58d68b8b0760e96ee14434b80ae1
Dbarbosa <danielebarbosaa56@gmail.com>
2024-11-19 16:47:15 -0300

    Correções na chamada do serviço realizado

--- src/main/java/com/api/salao/controllers/ServicoRealizadoController.java
+++ src/main/java/com/api/salao/controllers/ServicoRealizadoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/servicos-realizados")
+@RequestMapping("/servico-realizado")
 public class ServicoRealizadoController {
 
     @Autowired
